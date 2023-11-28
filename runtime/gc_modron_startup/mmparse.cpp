2ea1cf500ee07ec5b5342766c345c1a53f77b27d
Frank Kang <Frank.Kang@ibm.com>
2023-11-28 08:06:29 -0800

    add command line options for restore

--- runtime/gc_modron_startup/mmparse.cpp
+++ runtime/gc_modron_startup/mmparse.cpp
@@ -305,6 +305,34 @@ option_set_to_opt_integer(J9JavaVM* vm, const char* option, IDATA* optionIndex, 
  * @note optionIndex contains position of argument on command line if success returned, else -1
  */
 static IDATA
+option_set_to_opt_double_args(J9JavaVM* vm, const char* option, IDATA* optionIndex, IDATA match, double* address, J9VMInitArgs* args)
+{
+	IDATA returnCode = OPTION_OK;
+	double value = 0.0;
+
+	IDATA element = FIND_AND_CONSUME_ARG2(args, match, option, NULL);
+	*optionIndex = element;
+
+	if (element >= 0) {
+		returnCode = GET_DOUBLE_VALUE_ARGS(args, element, option, value);
+		if (OPTION_OK == returnCode) {
+			*address = value;
+		}
+	}
+	return returnCode;
+}
+
+/**
+ * Find, consume and record an option from the argument list.
+ * Given an option string and the match type, find the argument in the to be consumed list.
+ * If not found, return success.
+ * If found, consume it, verify the memory value.
+ *
+ * @return OPTION_OK if option is found and consumed or option not present, OPTION_MALFORMED if the option was malformed, OPTION_OVERFLOW if the option overflowed.
+ * @note value stored at address is invalid if failure returned
+ * @note optionIndex contains position of argument on command line if success returned, else -1
+ */
+static IDATA
 option_set_to_opt_double(J9JavaVM* vm, const char* option, IDATA* optionIndex, IDATA match, double* address)
 {
 	IDATA returnCode = OPTION_OK;
@@ -1339,6 +1367,21 @@ gcParseReconfigurableCommandLine(J9JavaVM* vm, J9VMInitArgs* args)
 			goto _error;
 		}
 		extensions->softMx = softmx;
+	}
+	if (-1 != FIND_ARG_IN_ARGS(args, EXACT_MEMORY_MATCH, "XXgc:fvtest_RAMSize=", NULL)) {
+		double testRAMSize = 0;
+		result = option_set_to_opt_double_args(vm, "XXgc:fvtest_RAMSize=", &index, EXACT_MEMORY_MATCH, &testRAMSize, args);
+		if (OPTION_OK != result) {
+			if (OPTION_MALFORMED == result) {
+				j9nls_printf(PORTLIB, J9NLS_ERROR, J9NLS_GC_OPTIONS_MUST_BE_NUMBER, "XXgc:fvtest_RAMSize");
+			}
+			goto _error;
+		}
+		if ((0 > extensions->testRAMSize) || (500 < extensions->testRAMSize)) {
+			j9nls_printf(PORTLIB, J9NLS_ERROR, J9NLS_GC_OPTIONS_INTEGER_OUT_OF_RANGE, "fvtest_RAMSize=", (UDATA)0, (UDATA)500);
+			goto _error;
+		}
+		j9tty_printf(PORTLIB, "Current testRam in restore is: %llu\n", extensions->testRAMSize);
 	}
 	if (!gcParseReconfigurableSoverignArguments(vm, args)) {
 		goto _error;
